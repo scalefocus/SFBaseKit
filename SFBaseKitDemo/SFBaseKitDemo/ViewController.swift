df52a0955d8a46946f44dc1ad29c207a58b3d31c
VeselaIl <vesela.ilchevska@scalefocus.com>
2020-04-09 17:10:21 +0300

    Initial commit

---
+++ SFBaseKitDemo/SFBaseKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseKitDemo
+//
+//  Created by Dimitar V. Petrov on 6.12.19.
+//  Copyright © 2019 Upnetix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
